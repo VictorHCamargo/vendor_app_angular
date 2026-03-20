fd916df3dfc18fc9cb42ebeba7edce12b5cb636a
VictorHCamargo <victor.h0786@gmail.com>
2026-03-19 22:17:26 -0300

    ajustes finais de novo kkkkk

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,14 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'category/form/:id',
-    renderMode: RenderMode.Client // Diz ao Angular: "Não tente fazer prerender disso no build"
-  },
-  {
-    path: 'group/form/:id',
-    renderMode: RenderMode.Client
-  },
-  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
